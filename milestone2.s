a134a81f623515be3330009b17700e6998dc1726
Zhuochen-Wu <70975240+Zhuochen-Wu@users.noreply.github.com>
2021-02-14 12:28:38 -0500

    Update milestone2.s

--- milestone2.s
+++ milestone2.s
@@ -1,3 +1,12 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+; Name: Put your name here for labs
+; Student Number: Put your student number here for labs
+; Lab Section: Put your lab section here for labs
+; Description of Code: Turns on User LED D4 with the push of an external button
+
+ 
 ; Original: Copyright 2014 by Jonathan W. Valvano, valvano@mail.utexas.edu
 
 
@@ -18,7 +27,7 @@ GPIO_PORTM_DIR_R             EQU 0x40063400  ;GPIO Port M Direction Register Add
 GPIO_PORTM_DEN_R             EQU 0x4006351C  ;GPIO Port M Direction Register Address (Fill in these addresses)
 GPIO_PORTM_DATA_R            EQU 0x400633FC  ;GPIO Port M Data Register Address      (Fill in these addresses) 
 
-COMBINATION EQU 2_0101
+COMBINATION EQU 2_1101
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;Do not alter this section
@@ -31,7 +40,7 @@ COMBINATION EQU 2_0101
 
 
 ;Function PortN_Init 
-PortN_Init
+PortN_Init 
     ;STEP 1
     LDR R1, =SYSCTL_RCGCGPIO_R 
     LDR R0, [R1]   
@@ -65,13 +74,13 @@ PortM_Init
     ;STEP 5
     LDR R1, =GPIO_PORTM_DIR_R   
     LDR R0, [R1] 
-    ORR R0,R0, #0x0          
+    ORR R0,R0, #0x00          
 	STR R0, [R1]   
     
 	;STEP 7
     LDR R1, =GPIO_PORTM_DEN_R   
     LDR R0, [R1] 
-    ORR R0, R0, #2_10001          
+    ORR R0, R0, #0x11          
 	                          
     STR R0, [R1]    
 	BX  LR                     
@@ -81,35 +90,43 @@ State_Init LDR R5,=GPIO_PORTN_DATA_R  ;Locked is the Initial State
 	       MOV R4,#2_00000001
 	       STR R4,[R5]
 	       BX LR 
-
+            
 Start                             
 	BL  PortN_Init                
 	BL  PortM_Init
 	BL  State_Init
 	LDR R3, =COMBINATION         ;R3 stores our combination
-	LDR R0, = GPIO_PORTM_DATA_R
+    LDR R0, = GPIO_PORTM_DATA_R
 Loop
-			MOV R6, #0x3 ; R6 is a counter FLAG to indicate the shift of bits also how many steps has been done. We execute the following 4 times, the value change in R6 should be 3,2,1,0. At the last time we excute the following loop, last bit shift left of 0 space.
-            
-			
-InputShift  
-      LDR R1,[R0]		
-			AND R7,R1,#2_00010000 ; R7 stores the current load flag, use mask to remove the interference of unwanted bits
-			CMP R7,#2_00010000;checking if it is in the load mode or idle mode
-			IT EQ ;giving condition of equal
-			ANDEQ R2,R1,#2_00000001 ;if equal, we masking bits to get the data ready to be shift in the next step
-			ANDNE R2,R1,#2_00000000 ;if not euqal, we remove all bits
-			LSL R9,R2,R6 ;we left shift bits with the space value stored in R6, Then stored the shifted value in R9
-			
-			SUBNE R6, #0x1 ;subtract R6 by 1,means we finish 1 cycle of this loop
-			CMP R6,#0x0
-			BGE InputShift
-			
-			
-			AND R9,#2_00001111
-			CMP R9,R3
-			BEQ Unlocked_State
-			BNE Locked_State
+         MOV R10, #0x0 ;a flag indicate which step we are in the overall sequential input of binary code
+         MOV R9,#0x0 ;the register that store the seqential input		 
+StatusCheck
+         LDR R1,[R0]		
+		 AND R7,R1,#2_00010000 ;masking off bits
+		 CMP R7,#2_00010000
+         IT EQ 
+		 BEQ InputShift
+         BNE StatusCheck
+InputShift
+         
+         AND R2,R1,#2_00000001 ;we use mask to only take last bit which is the input of sequential binary code
+         ORR R9,R2 ;copy the value of R2 first bit to R9	 
+		 LSL R9,R9,#0x1 ; we do logic shift left on R9's value
+		 ADD R10,#0x1
+         CMP R10,#0x4
+         IT EQ		 
+		 BEQ CheckSameValue	
+	     BNE StatusCheck
+
+CheckSameValue
+         LSR R9,R9,#0x1 ;we need to move back 1 space since the first bit in the InputShift loop has been move left to 5th location
+		 AND R9,R9,#2_00001111 ;remove interference by using the bit mask
+		 CMP R9,R3
+
+		 BEQ Unlocked_State
+		 BNE Locked_State
+ 
+	
  
 Locked_State                    
 	LDR R5,=GPIO_PORTN_DATA_R
@@ -124,4 +141,4 @@ Unlocked_State
 	B Loop
 	
 	ALIGN   
-    END
+    END  
